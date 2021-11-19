1b2d97dead84434481f2a49d69beb5961bae92d6
Salihin Subhan <48940069+salihinsubhan@users.noreply.github.com>
2021-11-19 15:50:45 -0800

    dss

--- tests/testthat/test-histredline.R
+++ tests/testthat/test-histredline.R
@@ -1,7 +1,7 @@
 test_that("histogram with mean line is generated properly", {
   data(iris)
   p <- histredline(iris, Sepal.Length)
-  expect_true(is.ggplot(p))
+  expect_true(dplyr::is.ggplot(p))
   expect_error(print(histredline(iris, Species)), NA)
   xx
   rm(iris)
