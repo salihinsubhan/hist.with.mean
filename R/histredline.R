d376448180b2a0f804627e57fbf3539cf34bbef7
Salihin Subhan <48940069+salihinsubhan@users.noreply.github.com>
2021-11-19 14:27:51 -0800

    new approach?

--- R/histredline.R
+++ R/histredline.R
@@ -2,8 +2,8 @@
 #'
 #' This function creates a histogram of a numeric variable and adds a red line denoting the position of the mean in the distribution of values of the variable.
 #'
-#' @param x Dataset which contains the numeric variable
-#' @param y Variable used to generate the histogram
+#' @param data Dataset which contains the numeric variable
+#' @param column Variable used to generate the histogram
 #'
 #' @return A histogram with a red line denoting the mean
 #' @export
@@ -11,17 +11,16 @@
 #' @examples
 #' histredline(mtcars, mpg)
 #' @importFrom ggplot2 ggplot aes geom_histogram geom_vline
-histredline <- function(x, y) {
-  calc <-
-  if(!is.data.frame(x)) {
-    stop("x should be a dataframe!")
+histredline <- function(data, column) {
+  if(!is.data.frame(data)) {
+    stop("data should be a dataframe!")
   }
-  if(!is.numeric({{ y }})) {
-    stop("y should be numeric!")
+  if(!is.numeric(data[,column])) {
+    stop("column should be numeric!")
   }
-  mean.y <- mean({{ y }}, na.rm=T)
+  mean.col <- mean(data[,column], na.rm=T)
   x %>%
-    ggplot2::ggplot(aes({{ y }})) +
+    ggplot2::ggplot(aes(data[,column])) +
     ggplot2::geom_histogram() +
-    ggplot2::geom_vline(aes(xintercept = mean.y), colour = "red")
+    ggplot2::geom_vline(aes(xintercept = mean.col), colour = "red")
 }
