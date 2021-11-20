776b527457075b266000cec392180bbfd89ada7a
Salihin Subhan <48940069+salihinsubhan@users.noreply.github.com>
2021-11-19 16:02:26 -0800

    finished tests

--- tests/testthat/test-histredline.R
+++ tests/testthat/test-histredline.R
@@ -2,8 +2,9 @@ test_that("histogram with mean line is generated properly", {
   data(iris)
   p <- histredline(iris, Sepal.Length)
   expect_true(dplyr::is.ggplot(p))
+  expect_identical(class(test.hist$layers[[1]]$geom)[1], "GeomBar")
+  expect_identical(class(test.hist$layers[[2]]$geom)[1], "GeomVline")
   expect_error(print(histredline(iris, Species)), NA)
-  xx
   rm(iris)
   rm(p)
 })
