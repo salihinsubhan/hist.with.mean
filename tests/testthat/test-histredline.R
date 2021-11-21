8d61bf4b769d3c3899df5b37c19df62e510ccda1
Salihin Subhan <48940069+salihinsubhan@users.noreply.github.com>
2021-11-20 18:24:34 -0800

    dds

--- tests/testthat/test-histredline.R
+++ tests/testthat/test-histredline.R
@@ -1,8 +1,8 @@
 test_that("histogram with mean line is generated properly", {
   data(iris)
   p <- histredline(iris, Sepal.Length)
-  expect_true(dplyr::is.ggplot(p))
+  expect_true(ggplot2::is.ggplot(p))
   expect_identical(class(p$layers[[1]]$geom)[1], "GeomBar")
   expect_identical(class(p$layers[[2]]$geom)[1], "GeomVline")
-  expect_error(print(histredline(iris, Species)), "Error in histredline(iris, Species) : column should be numeric!")
+  expect_error(print(histredline(iris, Species)), "column should be numeric!")
 })
